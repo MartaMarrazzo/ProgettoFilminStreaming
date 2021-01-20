bce1bedc82eb9fc22036f66c136a80ff482e9d75
IsabellaMarini <76102845+IsabellaMarini@users.noreply.github.com>
2021-01-20 11:49:55 +0100

    1

--- streamingmovies/src/main/java/it/univpm/streamingmoviesModel/StreamingWebsite.java
+++ streamingmovies/src/main/java/it/univpm/streamingmoviesModel/StreamingWebsite.java
@@ -8,27 +8,27 @@ import it.univpm.streamingmoviesService.StreamingWebsiteServiceIMPL;
 public class StreamingWebsite extends StreamingWebsiteServiceIMPL {
 	private String Name;
 	private String Country;
-	private float UpdateTime;
 	private String UpdateDate;
 	private String CreateDate;
 	private boolean isDead;
+	private int updateTime;
+	private int quantity;
 	ArrayList<StreamingWebsite> getStreamingWebsite(String url) {
 		return null;
 	}
 	
 	public StreamingWebsite( String Name,String Country, 
-			String UpdateDate,String CreateDate, boolean isDead ) {
+			String UpdateDate,String CreateDate, boolean isDead, int updateTime, int quantity) {
 		this.setName(Name);
 		this.setCountry(Country);
-		this.setUpdateTime(UpdateTime);
 		this.setUpdateDate(UpdateDate);
 		this.setCreateDate(CreateDate);
 		this.setDead(isDead);
+		this.setUpdateTime(updateTime);
+		this.setQuantity(quantity);
 	
 	}
-	
-	
-	
+
 	public String getName() {
 		return Name;
 	}
@@ -37,84 +37,52 @@ public class StreamingWebsite extends StreamingWebsiteServiceIMPL {
 		Name = name;
 	}
 
-
-
-
-
 	public String getCountry() {
 		return Country;
 	}
-
-
-
-
-
+	
 	public void setCountry(String country) {
 		Country = country;
 	}
-
-
-
-
-
-	public float getUpdateTime() {
-		return UpdateTime;
-	}
-
-
-
-
-
-	public void setUpdateTime(float updateTime) {
-		UpdateTime = updateTime;
-	}
-
-
-
-
 
 	public String getUpdateDate() {
 		return UpdateDate;
 	}
 
-
-
-
-
 	public void setUpdateDate(String updateDate) {
 		UpdateDate = updateDate;
 	}
-
-
-
-
 
 	public String getCreateDate() {
 		return CreateDate;
 	}
 
-
-
-
-
 	public void setCreateDate(String createDate) {
 		CreateDate = createDate;
 	}
-
-
-
-
 
 	public boolean isDead() {
 		return isDead;
 	}
 
-
-
-
-
 	public void setDead(boolean isDead) {
 		this.isDead = isDead;
+	}
+
+	public int getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(int updateTime) {
+		this.updateTime = updateTime;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
 	}
 
 }
