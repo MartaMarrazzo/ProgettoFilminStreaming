b43aaea8f677215074a3e9669519daa913bb3bc3
MartaMarrazzo <S1093316@studenti.univpm.it>
2021-01-21 17:24:00 +0100

    filter-javadoc

--- streamingmovies/src/main/java/it/univpm/streamingmoviesFilters/Filter.java
+++ streamingmovies/src/main/java/it/univpm/streamingmoviesFilters/Filter.java
@@ -5,8 +5,18 @@ import java.util.ArrayList;
 import org.json.simple.JSONObject;
 
 import it.univpm.streamingmoviesModel.StreamingWebsite;
-
+/**
+ * 
+ * @author marra
+ *
+ *classe Filter in cui definiamo tre arrayList destinati a contenere elementi
+ *filtratri per nazione di hosting(country), ultimo update (last update) e data di 
+ *creazione (first update)
+ *
+ *
+ */
 public class Filter {
+	
 protected String category;
 protected ArrayList<Filter> filteringCountries = new ArrayList<>();
 protected ArrayList<Filter> filteringFirstUpdates = new ArrayList<>();
@@ -15,8 +25,10 @@ protected ArrayList<Filter> filteringLastUpdates = new ArrayList<>();
 protected Filter(String category) {
 	this.category=category;
 }
+
 public Filter() {
 }
+
 public void toFilter(ArrayList<StreamingWebsite> ListToFilter) {};
 
 public ArrayList<Filter> getCountryFilter() {
@@ -31,7 +43,13 @@ public ArrayList<Filter> getCreateDateFilter() {
 	return filteringFirstUpdates;
 	}
 
-
+/**
+ * parseFilter verifica che il JSONObject contenga la chiave interessata
+ * (rispettivamente country,create_date,update_date affinchè queste vengano 
+ * aggiunte all' arraylist relativo
+ * 
+ * @param body
+ */
 public void parseFilter(JSONObject body) {
 	Filter f;
 	if (body.containsKey("country")) {
